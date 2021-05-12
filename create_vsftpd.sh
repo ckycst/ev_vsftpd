4074f0742987f072ecf9acd4e62b8cf7fbda58e8
Carlos <ckycst@gmail.com>
2021-05-12 09:52:33 +0800

    Update create_vsftpd.sh

--- create_vsftpd.sh
+++ create_vsftpd.sh
@@ -1,2 +1,17 @@
 #! /bin/bash
 echo y | sudo apt install vsftpd
+sudo netstat -nltp | grep 21
+sudo systemctl start vsftpd.service
+
+sudo mkdir /home/vsftpd/
+sudo touch /home/vsftpd/welcome.txt
+
+sudo useradd -d /home/vsftpd -s /bin/bash uftp
+sudo passwd uftp
+
+sudo rm /etc/pam.d/vsftpd
+ls /etc/pam.d/vsftpd
+
+sudo usermod -s /sbin/nologin uftp
+
+
